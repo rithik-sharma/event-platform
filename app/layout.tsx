8d148ff058b5bfc79061d83f13b3cdb4b0484e05
rithik-sharma <srithik645@gmail.com>
2024-04-24 10:35:48 +0530

    Footer changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
+  title: 'Event-Hub',
   description: 'Evently is a platform for event management.',
   icons: {
-    icon: '/assets/images/logo.svg'
+    icon: '/assets/images/logo.png'
   }
 }
 
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
+import React, { useState } from 'react';
 
 const Footer = () => {
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
   return (
     <footer className="border-t">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
@@ -14,7 +18,7 @@ const Footer = () => {
           />
         </Link>
 
-        <p>2023 Evently. All Rights reserved.</p>
+        <p>© {currentYear} EventHub. All Rights reserved.</p>
       </div>
     </footer>
   )
