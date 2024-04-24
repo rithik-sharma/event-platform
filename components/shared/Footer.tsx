fc7b52c46955370aad6d024a8897beaff2b383bf
rithik-sharma <srithik645@gmail.com>
2024-04-24 08:43:53 +0530

    logo added

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -7,9 +7,9 @@ const Footer = () => {
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href='/'>
           <Image 
-            src="/assets/images/logo.svg"
+            src="/assets/images/logo.png"
             alt="logo"
-            width={128}
+            width={134}
             height={38}
           />
         </Link>
