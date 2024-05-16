7528987fd59ea3c18593389b5a8c707831545811
rithik-sharma <srithik645@gmail.com>
2024-05-16 12:28:55 +0530

    Social Added!!

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -6,11 +6,19 @@ import React, { useState } from 'react';
 
 const Footer = () => {
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // Smooth scrolling behavior
+    });
+  };
+
   return (
     <footer className="border-t">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
-        <Link href='/'>
-          <Image 
+        <Link href="/">
+          <Image
             src="/assets/images/logo.png"
             alt="logo"
             width={134}
@@ -18,10 +26,25 @@ const Footer = () => {
           />
         </Link>
 
-        <p>© {currentYear} EventHub. All Rights reserved.</p>
+        
+
+      
+
+        <div className="flex justify-between items-center">
+  <p>© {currentYear} EventHub. All Rights reserved.</p>
+  {/* Move to Top button */}
+  <button onClick={scrollToTop} className="flex items-center gap-2 ml-2 hover:text-gray-400 animate-bounce">
+  {/* SVG icon for move to top */}
+  <img src="/assets/icons/up-arrow.png" alt="Feature Icon" className="w-8 h-8" /> {/* Small icon */}
+</button>
+</div>
+
+
+
+        
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer
--- components/ui/ContactSection.tsx
+++ components/ui/ContactSection.tsx
@@ -1,88 +1,154 @@
+ "use client"
 
-"use client"
-
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
-
-function ContactForm() {
-  const [state, handleSubmit] = useForm("mjvnzaar");
-
-  if (state.succeeded) {
-    return (
-      <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mx-auto max-w-sm mt-6">
-        <p className="text-center">Thanks for joining!</p>
-      </div>
-    );
-  }
-
-  return (
-    <form onSubmit={handleSubmit} className="relative max-w-md mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div className="mb-4">
-        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
-        <input
-          id="email"
-          type="email"
-          name="email"
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
-        <ValidationError
-          prefix="Email"
-          field="email"
-          errors={state.errors}
-          className="text-red-500 text-xs italic"
-        />
-      </div>
-      <div className="mb-6">
-        <label htmlFor="message" className="block text-gray-700 text-sm font-bold mb-2">Message</label>
-        <textarea
-          id="message"
-          name="message"
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-          className="text-red-500 text-xs italic"
-        />
-      </div>
-      <div className="flex items-center justify-between">
-        <button
-          type="submit"
-          disabled={state.submitting}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded focus:outline-none focus:shadow-outline"
-        >
-            <div className="flex items-center gap-2"> {/* Flex container to align icon and heading */}
-         <img src="/assets/icons/submit-icon.svg" alt="Feature Icon" className="w-10 h-10" /> {/* Small icon */}
-         <h6 className="h6-bold">Submit</h6>
-        </div>
-          
-        </button>
-
-      </div>
-      <p className='mt-4'>Want to contribute <span className='text-red-600'>?</span> <a className='hover:text-blue-400'target='_blank' href="https://github.com/rithik-sharma">Github</a></p>
+      import React from 'react';
+      import { useForm, ValidationError } from '@formspree/react';
       
-      {/* Absolute positioning for SVG image */}
-      <img src="/assets/images/moon.png" alt="Right Down Icon" className="absolute bottom-0 right-0 mb-4 mr-4 w-14 h-14" />
-    </form>
-  );
-}
-
-function ContactSection() {
-  return (
-    <section className="bg-[url('/assets/icons/contact-pattern.svg')] py-5 md:py-10">
-      <div className="wrapper justify-center content-center ">
-       <div className="flex items-center justify-center gap-2 mb-4"> {/* Flex container to align icon and heading */}
-         <img src="/assets/icons/contact.svg" alt="Feature Icon" className="w-10 h-10" /> {/* Small icon */}
-         <h2 className="text-3xl font-bold text-center">Contact Us</h2>
-        </div>
-        
-        <div className="max-w-2xl mx-auto">
-          <ContactForm />
-        </div>
-      </div>
-    </section>
-  );
-}
-
-export default ContactSection;
+      function ContactForm() {
+        const [state, handleSubmit] = useForm("mjvnzaar");
+      
+        if (state.succeeded) {
+          return (
+            <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mx-auto max-w-sm mt-6">
+        <p className="text-center">Thanks for joining!</p>
+      </div> 
+          );
+        }
+      
+        return (
+      
+          <form onSubmit={handleSubmit} className=" relative max-w-md mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
+              <input
+                id="email"
+                type="email"
+                name="email"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+                className="text-red-500 text-xs italic"
+              />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="message" className="block text-gray-700 text-sm font-bold mb-2">Message</label>
+              <textarea
+                id="message"
+                name="message"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+              <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+                className="text-red-500 text-xs italic"
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                type="submit"
+                disabled={state.submitting}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded focus:outline-none focus:shadow-outline"
+              >
+                  <div className="flex items-center gap-2"> {/* Flex container to align icon and heading */}
+               <img src="/assets/icons/submit-icon.svg" alt="Feature Icon" className="w-10 h-10" /> {/* Small icon */}
+               <h6 className="h6-bold">Submit</h6>
+              </div>
+                
+              </button>
+      
+            </div>
+            <p className='mt-4'>Want to contribute <span className='text-red-600'>?</span> <a className='hover:text-blue-400'target='_blank' href="https://github.com/rithik-sharma">Github</a></p>
+      
+            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 rounded-full p-3 mt-5 bg-white shadow-md hover:shadow-xl transition duration-300">
+                <a
+                  href="https://www.instagram.com/shh_rithik_/"
+                  target="_blank"
+                  role="button"
+                  className="rounded-full transform hover:-translate-y-1 hover:shadow-md transition duration-300"
+                >
+                  <span className="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#c13584]">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                      <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+                    </svg>
+                  </span>
+                </a>
+      
+                {/* Github  */}
+                <a
+                  href="https://github.com/rithik-sharma"
+                  target="_blank"
+                  role="button"
+                  className="rounded-full transform hover:-translate-y-1 hover:shadow-md transition duration-300"
+                >
+                  <span className="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#333]">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+                      <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+                    </svg>
+                  </span>
+                </a>
+      
+                {/* Linkedin */}
+                <a
+                  href="https://www.linkedin.com/in/rithik-sharma-/"
+                  target="_blank"
+                  role="button"
+                  className="rounded-full transform hover:-translate-y-1 hover:shadow-md transition duration-300"
+                >
+                  <span className="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#0077b5]">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                      <path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z" />
+                    </svg>
+                  </span>
+                </a>
+      
+                {/* twitter */}
+                <a
+                  href="https://twitter.com/RithikS725"
+                  target="_blank"
+                  role="button"
+                  className="rounded-full transform hover:-translate-y-1 hover:shadow-md transition duration-300"
+                >
+                  <span className="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-black">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 512 512"
+                    >
+                      <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+                    </svg>
+                  </span>
+                </a>
+              </div>
+            
+            {/* Absolute positioning for SVG image */}
+            <img src="/assets/images/moon.png" alt="Right Down Icon" className="absolute bottom-1 right-0 mb-4 mr-4 w-14 h-14" />
+      
+            </div>
+          </form>
+          
+        );
+      }
+      
+      function ContactSection() {
+        return (
+          <section className="bg-[url('/assets/icons/contact-pattern.svg')] py-5 md:py-10">
+            <div className="wrapper justify-center content-center ">
+             <div className="flex items-center justify-center gap-2 mb-4"> {/* Flex container to align icon and heading */}
+               <img src="/assets/icons/contact.svg" alt="Feature Icon" className="w-10 h-10" /> {/* Small icon */}
+               <h2 className="text-3xl font-bold text-center">Contact Us</h2>
+              </div>
+              
+              <div className="max-w-2xl mx-auto">
+                <ContactForm />
+              </div>
+            </div>
+          </section>
+        );
+      }
+      
+      export default ContactSection;
+      
